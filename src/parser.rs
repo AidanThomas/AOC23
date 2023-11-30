b9e0dfd341a34f97cd3b623bf05b7f0f767f12ed
Aidan Thomas <aidant@agylia.com>
2023-11-30 14:12:16 +0000

    Improve parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,10 @@
 use std::fs;
 
 pub fn read_by_line(path: &str) -> Result<Vec<String>, String> {
-    let mut result = Vec::new();
-
     let contents = match fs::read_to_string(path) {
         Ok(x) => x,
         Err(e) => return Err(e.to_string()),
     };
 
-    for line in contents.lines() {
-        result.push(line.to_string());
-    }
-
-    Ok(result)
+    Ok(contents.lines().map(|x| x.to_string()).collect())
 }
